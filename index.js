72086f8f13c32785230048138512e79314d45dac
Joe <zvmboni@gmail.com>
2022-03-17 09:49:25 -0700

    commit tree 460694878f7da739b2a09d6fbacf7cd8ef564133

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+function findMatching(driverArray, driverName){
+    //const matches = driverArray.filter(driver => driver === driverName);
+
+    const matches = driverArray.filter((driver) => {
+        return (driver               === driverName               ||
+                driver.toLowerCase() === driverName.toLowerCase() ||
+                driver.toUpperCase() === driverName.toUpperCase()
+               );
+    });
+
+    return matches;
+}
+
+function fuzzyMatch(driverArray, driverName){
+    const matches = driverArray.filter((driver) => {
+        return (driver[0]               === driverName[0]               ||
+                driver[0].toLowerCase() === driverName[0].toLowerCase() ||
+                driver[0].toUpperCase() === driverName[0].toUpperCase()
+               );
+    });
+
+    return matches;
+}
+
+function matchName(driverArray, driverName){
+    const matches = driverArray.filter((driver) => {
+        return driver.name === driverName;
+    })
+
+    return matches;
+};
